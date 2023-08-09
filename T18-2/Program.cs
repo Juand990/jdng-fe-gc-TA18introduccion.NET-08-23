04d9c514c9f24142942a76f7b61ef912fe4b49de
Juand990 <jdng990@gmail.com>
2023-08-09 16:02:14 +0200

    nuevos proyectos .net

---
+++ T18-2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int num1 = 5;
+int numero2 = 6;
+double numero3 = 2.57;
+double suma;
+suma = num1 + numero3;
+
+
+Console.WriteLine(suma);
+Console.WriteLine("Introduce un valor:");
+string valor1 = Console.ReadLine();
+Console.WriteLine("El numero introducido es: "+valor1);
+int valorint = Convert.ToInt32(valor1);
+Console.ReadKey();
+
+
---
+++ T18/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Console.ReadKey(); //Para la ejecucion hasta presionar una tecla
